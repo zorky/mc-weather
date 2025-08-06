697b566d29b6c9a34c76b47123be7e64864ff6be
Olivier DUVAL <zorky00@gmail.com>
2025-08-06 17:50:20 +0200

    ajout tools prix crypto monnaie

--- agent/agent.py
+++ agent/agent.py
@@ -2,15 +2,16 @@ import os
 from langchain.agents import initialize_agent, AgentType
 from langchain_community.chat_models import ChatOpenAI
 
+from mcp_server.crypto_price import get_crypto_price
 from mcp_server.weather_tools import get_weather
 # from mcp_server.geo_tools import get_coordinates_openstreetmap, get_coordinates_openmeteo
 
 MODEL=os.getenv("MODEL", "llama3:8b-instruct-q4_K_M")
 LLM_API=os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1")
 
-LLM_TEMPERATURE=0  # 0 : déterministe et précis, 0.3 : un peu plus créatif
+LLM_TEMPERATURE=0.3  # 0 : déterministe et précis, 0.3 : un peu plus créatif
 
-tools = [get_weather]
+tools = [get_weather, get_crypto_price]
 # tools = [get_weather, get_coordinates_openstreetmap]
 # tools = [get_coordinates_openmeteo, get_weather]
 
---
+++ mcp_server/crypto_price.py
@@ -0,0 +1,29 @@
+import requests
+
+from langchain.tools import tool
+
+import logging
+from logger import init_logger
+logger = init_logger(level=logging.DEBUG)
+
+@tool
+def get_crypto_price(symbol: str) -> str:
+    """Donne le cours actuel d'une crypto (ex: BTC, ETH)."""
+    logger.debug(f"Crypto price pour {symbol}")
+    # https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR&e=CCCAGG
+    url = f"https://min-api.cryptocompare.com/data/price?fsym={symbol.lower()}&tsyms=EUR,USD&e=CCCAGG"
+    # params = {"fsym": symbol.lower(), "tsyms": "EUR,USD", "e": "CCCAGG"}
+    logger.debug(f"url: {url}")
+    # logger.debug(f"params: {params}")
+    try:
+        # r = requests.get(url, params=params)
+        r = requests.get(url)
+        data = r.json()
+        price = data.get(symbol.lower(), {}).get("EUR")
+        if price:
+            return_response = f"Le cours de {symbol.upper()} est de {price} €"
+            logger.debug(return_response)
+            return return_response
+        return f"Crypto non trouvée : {symbol}"
+    except Exception as e:
+        return f"Erreur crypto : {str(e)}"
